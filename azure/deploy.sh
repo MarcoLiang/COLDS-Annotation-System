ebdca15a365383d800ad2487c0b2cdbc77643e93
Aaron Green <aarongg2@illinois.edu>
2019-02-05 19:39:38 -0600

    upd deploy

--- azure/deploy.sh
+++ azure/deploy.sh
@@ -1,14 +1,2 @@
-#!/bin/bash
-set -e
-
-docker-machine create \
-    --driver azure \
-    --azure-resource-group CLaDS \
-    --azure-size $1 \
-    --azure-vnet CLaDS-vnet \
-    --azure-subnet default \
-    --azure-subnet-prefix 10.0.0.0/24 \
-    --azure-private-ip-address 10.0.0.12 \
-    --azure-open-port 80 \
-    --azure-custom-data cloud-init.txt \
-    clads-annotation-system
+eval $(docker-machine env clads-annotation-system)
+docker-compose up -d --no-deps --build mainapp
--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,4 @@ from server import app
 import os
 
 if __name__=="__main__":
-    os.environ['ENV'] = 'prod'
     app.run()
