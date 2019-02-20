da6fb22909f3f082509189a65160b50c7c3d8155
Aaron Green <aarongg2@illinois.edu>
2019-02-20 10:15:36 -0600

    allow zipfile upload

--- api/uploadAPI.py
+++ api/uploadAPI.py
@@ -10,7 +10,7 @@ from schema.User import User
 from schema.Document import Document
 from util.userAuth import login_auth_required
 
-import os, json
+import os, json, zipfile
 
 env = os.environ["APP_ENV"]
 cfg = json.loads(open('config.json').read())[env]
@@ -25,107 +25,123 @@ class UploadAPI(Resource):
 	"""
 	@login_auth_required
 	def get(self):
-		return make_response(
-			render_template(
-				'upload_file.html',
-				author=User.objects(id=session["user_id"]).first()
-			),
-			200,
-			{'Content-Type': 'text/html'}
-		)
+            return make_response(
+                render_template(
+                    'upload_file.html',
+                    author=User.objects(id=session["user_id"]).first()
+                ),
+                200,
+                {'Content-Type': 'text/html'}
+            )
 
 	@login_auth_required
 	def post(self):
-		args = parser.parse_args()
-		ds_name = args['ds_name']
-		ds_privacy = args['ds_privacy']
+            args = parser.parse_args()
+            ds_name = args['ds_name']
+            ds_privacy = args['ds_privacy']
 
-		uploaded_files = request.files.getlist("file")
+            uploaded_files = request.files.getlist("file")
+            
+            is_zipfile = \
+                    len(uploaded_files) == 1 and \
+                    uploaded_files[0].filename[-4:] == ".zip"
 
-		err = self._validate_files(uploaded_files)
-		if err:
-			return make_response(jsonify(err), 200)
-		
-		owner = User.objects(id=session["user_id"]).first()
-		owner_path = self._get_or_init_owner_path()
-		err, ds_path = self._get_or_init_ds_path(owner_path, ds_name)
+            if is_zipfile:
+                zipfile = uploaded_files[0]
 
-		if err:
-			return make_response(jsonify(err), 200)
+            if not is_zipfile:
+                err = self._validate_files(uploaded_files)
+                if err:
+                    return make_response(jsonify(err), 200)
+            
+            owner = User.objects(id=session["user_id"]).first()
+            owner_path = self._get_or_init_owner_path()
+            err, ds_path = self._get_or_init_ds_path(owner_path, ds_name)
 
-		dataset = self._save_dataset_entry(ds_name, owner, ds_privacy)
-		self._save_dataset_files(uploaded_files, ds_path, dataset)
-		self._save_dataset_configs(ds_path)
+            if err:
+                return make_response(jsonify(err), 200)
 
-		return make_response(
-			jsonify({"message": "Files have been successfully uploaded"}),
-			200
-		)
+            dataset = self._save_dataset_entry(ds_name, owner, ds_privacy)
+            self._save_dataset_files(uploaded_files, ds_path, dataset, is_zipfile)
+            self._save_dataset_configs(ds_path)
+
+            return make_response(
+                jsonify({"message": "Files have been successfully uploaded"}),
+                200
+            )
 
 
 	def _validate_files(self, files):
-		for file in files:
-			if not file or not allowed_file(file.filename):
-				return {
-					"message": "File(s) format is(are) not correct"
-				}
+	    for file in files:
+		if not file or not allowed_file(file.filename):
+		    return {
+			"message": "File(s) format is(are) not correct"
+		    }
 
-		return None
+	    return None
 
 
 	def _get_or_init_owner_path(self):
-		owner = User.objects(id=session["user_id"]).first()
-		owner_path = cfg["dataset_base_path"] + str(owner.gitlab_id)
-		
-		if not os.path.exists(owner_path):
-			os.mkdir(owner_path)
+            owner = User.objects(id=session["user_id"]).first()
+            owner_path = cfg["dataset_base_path"] + str(owner.gitlab_id)
+            
+            if not os.path.exists(owner_path):
+                os.mkdir(owner_path)
 
-		return owner_path
+            return owner_path
 
 
 	def _get_or_init_ds_path(self, owner_path, ds_name):
-		ds_path = owner_path + "/" + ds_name + "/"
-		if os.path.isdir(ds_path):
-			return { "message": "Dataset already exists" }, ds_path
+            ds_path = owner_path + "/" + ds_name + "/"
+            if os.path.isdir(ds_path):
+                return { "message": "Dataset already exists" }, ds_path
 
-		os.mkdir(ds_path)
-		return None, ds_path
+            os.mkdir(ds_path)
+            return None, ds_path
 
 
 	def _save_dataset_entry(self, ds_name, owner, ds_privacy):
-		ds = Dataset()
-		ds.name = ds_name
-		ds.owner = owner
-		ds.privacy = ds_privacy 
-		ds.save()
-		return ds
+            ds = Dataset()
+            ds.name = ds_name
+            ds.owner = owner
+            ds.privacy = ds_privacy 
+            ds.save()
+            return ds
 
 
-	def _save_dataset_files(self, files, ds_path, dataset):
-		for file in files:
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(ds_path, filename))
+	def _save_dataset_files(self, files, ds_path, dataset, is_zipfile):
+            if not is_zipfile:
+                for file in files:
+                    filename = secure_filename(file.filename)
+                    file.save(os.path.join(ds_path, filename))
+            else:
+                fzip = files[0]
+                fzip.save('/tmp/' + fzip.filename)
+                zip_ref = zipfile.ZipFile('/tmp/' + fzip.filename, 'r')
+                zip_ref.extractall(ds_path)
+                zip_ref.close()
+                os.remove('/tmp/' + fzip.filename)
 
-			document = Document()
-			document.name = file.filename
-			document.dataset = dataset
-			document.save()
-
+            for fname in os.listdir(ds_path):
+                document = Document()
+                document.name = fname
+                document.dataset = dataset
+                document.save()
 
 	def _save_dataset_configs(self, ds_path):
-		if len(os.listdir(ds_path)) != 0:
-			file_corpus = open(ds_path + "/dataset-full-corpus.txt", "w")
-			file_toml = open(ds_path + "/file.toml", "w")
-			file_metadata = open(ds_path + "/metadata.data", "w")
+            if len(os.listdir(ds_path)) != 0:
+                file_corpus = open(ds_path + "/dataset-full-corpus.txt", "w")
+                file_toml = open(ds_path + "/file.toml", "w")
+                file_metadata = open(ds_path + "/metadata.data", "w")
 
-			for file in os.listdir(ds_path):
-				if file[-3:] == "txt" and file != "dataset-full-corpus.txt":
-					file_corpus.write("[None] " + file + "\n")
-					file_metadata.write(file + " " + file[:-4] + "\n")
+                for file in os.listdir(ds_path):
+                    if file[-3:] == "txt" and file != "dataset-full-corpus.txt":
+                        file_corpus.write("[None] " + file + "\n")
+                        file_metadata.write(file + " " + file[:-4] + "\n")
 
-			file_toml.write("type = \"file-corpus\"" + "\n")
-			file_toml.write("list = \"dataset\"")
+                file_toml.write("type = \"file-corpus\"" + "\n")
+                file_toml.write("list = \"dataset\"")
 
-			file_corpus.close()
-			file_toml.close()
-			file_metadata.close()
+                file_corpus.close()
+                file_toml.close()
+                file_metadata.close()
